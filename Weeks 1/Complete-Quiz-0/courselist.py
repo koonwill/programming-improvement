b5269112655ceb54405449ec19ca1637c15a51ff
koonwill <93331101+koonwill@users.noreply.github.com>
2022-09-06 23:58:25 +0700

    Implement Problem1,2 in courselist

---
+++ Weeks 1/Complete-Quiz-0/courselist.py
@@ -0,0 +1,20 @@
+class CourseList:
+    def __init__(self) -> None:
+        self.cl = {}
+
+    def add_course(self, course):
+        if course.course_id not in self.cl:
+            self.cl[course.course_id] = course
+            return True
+        return False
+    def drop_course(self, course_id):
+        if course_id in self.cl:
+            self.cl.pop(course_id)
+            return True
+        return False
+
+    def get_credits(self):
+        all_credits = 0
+        for course in self.cl.values():
+            all_credits += course.credits
+        return all_credits
